5ee24f48982c572dd5b615dd3338275c50e434cd
Kelden Lin <keldenlin@gmail.com>
2020-04-28 17:34:14 -0700

    Init

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// Populates the HTML with data
+// Takes in a string of pokemon name
+function populatePokemon(pokemon) {
+    // API Source: https://pokeapi.co/
+    // HINT: Use returned API data and run 
+    //       setFrontSprite(getFrontSprite(data))
+    //       to set the image
+}
+
+// Takes in data from populatePokemon()
+// Returns the image url for Pokemon Front Sprite 
+function getFrontSprite(pokeData) {}
+
+// Takes in image url from getFrontSprite()
+// Sets the image source in #pokemon-img 
+function setFrontSprite(pokeImg) {}
