f488df653be5f55ddac9760a3f18cd7622a91654
jerryiscat <94934251+jerryiscat@users.noreply.github.com>
2024-09-05 04:12:15 +0800

    finish the task

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,34 @@
 // Populates the HTML with data
 // Takes in a string of pokemon name
-function populatePokemon(pokemon) {
+async function populatePokemon(pokemon) {
     // API Source: https://pokeapi.co/
     // HINT: Use returned API data and run 
     //       setFrontSprite(getFrontSprite(data))
     //       to set the image
+    try {
+        //  fetch the data from api, putting the pokemon name into the url
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
+        const data = await response.json();
+        const imageUrl = getFrontSprite(data);
+        setFrontSprite(imageUrl);
+    } catch (error) {
+        console.error('error fetching data:', error);
+    }
+
 }
 
 // Takes in data from populatePokemon()
 // Returns the image url for Pokemon Front Sprite 
-function getFrontSprite(pokeData) {}
+function getFrontSprite(pokeData) {
+    // find the correct path from the https://pokeapi.co/ 
+    return pokeData.sprites.front_default
+}
 
 // Takes in image url from getFrontSprite()
 // Sets the image source in #pokemon-img 
-function setFrontSprite(pokeImg) {}
+function setFrontSprite(pokeImg) {
+    // find the image id from  the html file, and get the element, then change the image source
+    const element = document.getElementById('pokemon-img');
+    element.src = pokeImg;
+
+}
